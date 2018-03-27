27c390ac544b0b766cef331f79bcff85b74c8121
Marcus Hallman <hallman@wustl.edu>
2018-03-27 13:50:46 -0500

    First Commit

---
+++ Omega.R
@@ -0,0 +1,9 @@
+gdata<-list(c(1,1),c(1,2),c(1,3),c(1,4),c(2,1),c(2,3),c(2,4),c(3,1),c(3,4),c(4,1),c(4,2),c(4,3),c(4,4))
+omega<-function(glist,sigma){
+  matrix(data = NA, nrow=(length(glist)),ncol=(length(glist)))
+  for(i in 1:(length(glist))){
+    for (j in 1:(length(glist))){
+      
+    }
+  }
+}
