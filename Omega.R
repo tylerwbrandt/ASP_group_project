ba38aff95be61226b494f75d70b27532457d9d8c
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-27 13:59:12 -0500

    Turn list into dataframe
    G data is now in data frame with three columns (first docid, second docid, chosen {1,-1})

--- Omega.R
+++ Omega.R
@@ -1,4 +1,8 @@
-gdata<-list(c(1,1),c(1,2),c(1,3),c(1,4),c(2,1),c(2,3),c(2,4),c(3,1),c(3,4),c(4,1),c(4,2),c(4,3),c(4,4))
+gdata <- data.frame(c(1,1,1,2,2,2,3,3,4,4,4), c(2,3,4,1,3,4,1,4,1,2,3),
+                    c(1,1,1,-1,-1,-1,-1,1,-1,1,-1))
+colnames(gdata) <- c("first", "second", "y")
+View(gdata)
+
 omega<-function(glist,sigma){
   matrix(data = NA, nrow=(length(glist)),ncol=(length(glist)))
   for(i in 1:(length(glist))){
