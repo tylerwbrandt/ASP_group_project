7557c3c5ede8693492ec45f4570fa822cdb6443d
Gangyi Sun <gangyi.sun@wustl.edu>
2018-03-29 13:45:03 -0500

    Added equation 6

--- logLikelihood.R
+++ logLikelihood.R
@@ -9,6 +9,17 @@ Lg<-function(y,g){
   return(output)
 }
 
+derivativeLg<-function(y,g){
+  denom<-Lg(y,g)
+  output<-c()
+  for (i in 1:length(y)){
+    num<-y[i]*dnorm(g[i])
+    output[i]<-num/denom
+  }
+  return(output)
+}
+
 y1<-c(1,1,-1,1,-1)
 g1<-c(2,3,4,5,6)
 Lg(y1,g1)
+derivativeLg(y1,g1)
