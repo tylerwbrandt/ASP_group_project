6e30aeda031ac136f528c1bcc43d543942edfe60
Gangyi Sun <gangyi.sun@wustl.edu>
2018-03-29 13:31:51 -0500

    LogLikelihood Function
    First draft of LogLikelihood Function

---
+++ logLikelihood.R
@@ -0,0 +1,14 @@
+Lg<-function(y,g){
+  result<-c()
+  for (i in 1:length(y)){
+    x<-y[i]*g[i]
+    xi<-log(pnorm(x,0,1))
+    result<-c(result,xi)
+  }
+  output<-sum(result)
+  return(output)
+}
+
+y1<-c(1,1,-1,1,-1)
+g1<-c(2,3,4,5,6)
+Lg(y1,g1)
