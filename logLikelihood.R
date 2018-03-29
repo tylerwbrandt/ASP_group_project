b766820f93acf464c74e7f9e4d4d16d55c94898e
Gangyi Sun <gangyi.sun@wustl.edu>
2018-03-29 13:52:37 -0500

    added equation 7

--- logLikelihood.R
+++ logLikelihood.R
@@ -19,7 +19,20 @@ derivativeLg<-function(y,g){
   return(output)
 }
 
+secondDerivativeLg<-function(y,g){
+  LL<-Lg(y,g)
+  derivativeLL<-derivativeLg(y,g)
+  output<-diag(x=0,length(y),length(y))
+  for (i in 1:length(y)){
+    firstTerm<- ((dnorm(g[i]))^2)/LL^2
+    secondTerm<-g[i]*derivativeLL[i]
+    output[i,i]<-(-1*firstTerm)-secondTerm
+  }
+  return(output)
+}
+
 y1<-c(1,1,-1,1,-1)
 g1<-c(2,3,4,5,6)
 Lg(y1,g1)
 derivativeLg(y1,g1)
+secondDerivativeLg(y1,g1)
