494b88e1960eb88acb444ec6005f1472b9ad2aa9
Gangyi Sun <gangyi.sun@wustl.edu>
2018-04-02 19:17:35 -0500

    started devFile

---
+++ DevelopmentFile.R
@@ -0,0 +1,14 @@
+## ASP_Group_Project 
+## Development File 
+
+## Load libraries
+library(devtools)
+library(roxygen2)
+library(testthat)
+
+## Set working directory
+setwd("~/Documents/GitHub/ASP_group_project")
+
+## Creates package skeleton
+# Only run this the first time to create the skeleton 
+devtools::create("gaussianComparisons")
