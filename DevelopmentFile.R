be4b81f4556ed772b04019687e7d0d09a3d88944
Gangyi Sun <gangyi.sun@wustl.edu>
2018-04-02 19:22:05 -0500

    added code to devFile for package compilation

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -12,3 +12,14 @@ setwd("~/Documents/GitHub/ASP_group_project")
 ## Creates package skeleton
 # Only run this the first time to create the skeleton 
 # Testdevtools::create("gaussianComparisons")
+
+## Updates the package based on R scripts written, man and NAMESPACE automatically updates. 
+# Manually update DESCRIPTION if need be.
+current.code <- as.package("gaussianComparisons")
+load_all(current.code)
+document(current.code)
+check(current.code)
+
+
+## Sample code to test that functions work 
+
