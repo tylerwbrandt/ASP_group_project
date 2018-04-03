7ce6dc5ef84a6e3bf0d99ee97d9992568594968f
Gangyi Sun <gangyi.sun@wustl.edu>
2018-04-02 20:22:38 -0500

    Added roxygen code to d2Psi.R

--- gaussianComparisons/R/d2psi.R
+++ gaussianComparisons/R/d2psi.R
@@ -1,5 +1,19 @@
+#' Second derivative of the log of the numerator of the full posterior function (Equation 9)
+#'
+#' Adds to functions that are already created (derivative of log prior, derivative of log likelihood)
+#'
+#' @param omega A matrix which contains information about how a document is compared with all other documents.
+#' @param g An unknown vector of real numbers 
+#' @param y A vector of outcomes of pairwise comparison of documents 
+#'
+#' @return A vector representing the second derivative of psi 
+#' @author Group <\email{group@@wustl.edu}
+#' 
+#' @rdname d2Psi
+#' @export
+#' 
 d2psi<- function(omega,g, y){  
-    output<-(d2LogLike(y,g)+d2logpri(omega,g))
-           return(output)
+    output<-(d2LogLike(g,y)+d2logpri(omega,g))
+    return(output)
 }
 
