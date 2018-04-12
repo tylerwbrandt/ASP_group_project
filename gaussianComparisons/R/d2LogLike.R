08aa9b672b3bc0f2d4076bd12896ac46e781bc20
Tyler Brandt <tylerbrandt02@gmail.com>
2018-04-12 14:39:55 -0500

    rhoSquaredMaker Update
    Still haven't resolved negative issue

--- gaussianComparisons/R/d2LogLike.R
+++ gaussianComparisons/R/d2LogLike.R
@@ -12,12 +12,12 @@
 #' @export
 #' 
 d2LogLike<-function(g,y){
-  derivativeLL<-dLogLike(g,y)
-  output<-diag(x=0,length(y),length(y))
+  derivativeLL <- dLogLike(g,y)
+  output <- diag(x=0, length(y), length(y))
   for (i in 1:length(y)){
-    firstTerm<- ((dnorm(g[i]))^2)/(pnorm(y[i]*g[i])^2)
-    secondTerm<-g[i]*derivativeLL[i]
-    output[i,i]<-(-1*firstTerm)-secondTerm
+    firstTerm <- ((dnorm(g[i]))^2)/(pnorm(y[i]*g[i])^2)
+    secondTerm <- g[i]*derivativeLL[i]
+    output[i,i] <- (-1*firstTerm) - secondTerm
   }
   return(output)
 }
--- gaussianComparisons/R/d2LogPri.R
+++ gaussianComparisons/R/d2LogPri.R
@@ -13,9 +13,10 @@
 #' 
 d2LogPri<-function(omega,g){
   if (det(omega) == 0){
-  diag(omega)<-diag(omega)+.01}
-  inv_omega<-(solve(omega))
-  neg_inv_omega<- -1*(inv_omega)
+    diag(omega) <- diag(omega)+.01
+  }
+  inv_omega <- (solve(omega))
+  neg_inv_omega <- -1*(inv_omega)
   return(neg_inv_omega)
 }
 
